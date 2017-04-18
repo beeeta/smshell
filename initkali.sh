a5e07378d86f9e6b97f1f1d635d77974fcc30230
root <root@localhost.localdomain>
2017-04-18 02:38:23 -0400

    add msg

--- initkali.sh
+++ initkali.sh
@@ -19,6 +19,17 @@ echo 'userlist_file=/etc/vsftpd.userlist' >> /etc/vsftpd.conf
 echo 'userlist_enable=YES' >> /etc/vsftpd.conf
 echo 'userlist_deny=NO' >> /etc/vsftpd.conf
 
+# enable to upload file
+echo 'write_enable=YES' >> /etc/vsftpd.conf
+echo 'allow_writeable_chroot=YES' >> /etc/vsftpd.conf
+
+# disable user to get out of the path
+echo 'chroot_local_user=YES' >> /etc/vsftpd.conf
+
+# the save file path /home/user_name/ftp
+echo 'user_sub_token=$USER' >> /etc/vsftpd.conf
+echo 'local_root=/home/$USER/ftp' >> /etc/vsftpd.conf
+
 touch /etc/vsftpd.userlist
 echo 'itftp' > /etc/vsftpd.userlist
 
